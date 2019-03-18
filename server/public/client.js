07a35960dc593c0dd55e59f02f6a66232711252b
Dev Jana <dev@primeacademy.io>
2019-03-18 10:01:50 -0500

    object sent to server

--- server/public/client.js
+++ server/public/client.js
@@ -1,13 +1,34 @@
 $( document ).ready( readyNow );
 
+var currentCalculation = {
+    num1: 0,
+    operation: '',
+    num2: 0
+}
+
 function calculate(){
     console.log( 'in calculate' );
+    // get user inputs, combine with selected operation, create an object
+    currentCalculation.num1 = $( '#num1In' ).val();
+    currentCalculation.num2 = $( '#num2In' ).val();
+    console.log( 'currentCalculation:', currentCalculation );
+    // send object to server via POST
+    $.ajax({
+        type: 'POST',
+        url: '/calculate',
+        data: currentCalculation
+    }).then( function( response ){
+        console.log( 'back from server with:', response );
+    })
+    // display answer
+    // update history on DOM
 } // end calculate
 
 function clearInputs(){
     console.log( 'in clearInputs' );
     $( '#num1In' ).val( '' );
     $( '#num2In' ).val( '' );
+    currentCalculation.operation = '';
 } // end clearInputs
 
 function readyNow(){
@@ -17,5 +38,7 @@ function readyNow(){
 } // end readyNow
 
 function selectOperation(){
-    console.log( 'in selectOperation' );
+    console.log( 'in selectOperation', $( this ).text() );
+    currentCalculation.operation = $( this ).text();
+    console.log( 'currentOperation:', currentCalculation );
 } // end selectOperation
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 // requires
 const express = require( 'express' );
 const app = express();
-
+const bodyParser = require( 'body-parser' );
 // uses
 app.use( express.static( 'server/public' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
 
 // globals
 const port = 5000;
@@ -12,3 +13,8 @@ const port = 5000;
 app.listen( port, ()=>{
     console.log( 'server up on:', port );
 }) // end server up
+
+app.post( '/calculate', ( req, res )=>{
+    console.log( 'in /calculate POST', req.body );
+    res.send( 'meow' );
+}) // // end POST calculate
