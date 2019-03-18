e67ad568bff12f103757c06a3b0035d030f22f44
Dev Jana <dev@primeacademy.io>
2019-03-18 09:18:58 -0500

    client server ready for work

---
+++ server/server.js
@@ -0,0 +1,14 @@
+// requires
+const express = require( 'express' );
+const app = express();
+
+// uses
+app.use( express.static( 'server/public' ) );
+
+// globals
+const port = 5000;
+
+// spin up server
+app.listen( port, ()=>{
+    console.log( 'server up on:', port );
+}) // end server up
