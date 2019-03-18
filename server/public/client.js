0a533e2fe96d3b56ea1095afd7e6105a96fe2abb
Dev Jana <dev@primeacademy.io>
2019-03-18 09:29:04 -0500

    interface working

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,21 @@
 $( document ).ready( readyNow );
 
+function calculate(){
+    console.log( 'in calculate' );
+} // end calculate
+
+function clearInputs(){
+    console.log( 'in clearInputs' );
+    $( '#num1In' ).val( '' );
+    $( '#num2In' ).val( '' );
+} // end clearInputs
+
 function readyNow(){
-    console.log( 'JQ' );
+    $( '#equalsButton' ).on( 'click', calculate );
+    $( '#clearButton' ).on( 'click', clearInputs );
+    $( '.operationSelector' ).on( 'click', selectOperation );
 } // end readyNow
+
+function selectOperation(){
+    console.log( 'in selectOperation' );
+} // end selectOperation
